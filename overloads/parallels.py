32a4053b3541a80ef44b30599f2c16cc4015ddbe
Andy <3772259@gmail.com>
2021-01-17 15:04:29 +0800

    modify output of parfor

--- overloads/parallels.py
+++ overloads/parallels.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import builtins
 import multiprocessing.pool
-from typing import (Any, Callable, Dict, List, Optional, Sequence, Tuple, TypeVar, Union)
+from typing import (Any, Callable, Dict, List, Optional, Sequence, Text, Tuple, TypeVar, Union)
 
 import psutil  # type: ignore
-from mypy_extensions import DefaultNamedArg
 
 from capture_exceptions import Captured_Exception, capture_exceptions
 
@@ -20,8 +19,8 @@ def launch_parpool() -> None:
     pool = multiprocessing.pool.Pool(processes)
 
 
-def print(*args: Any, end: str = '\n') -> None:
-    output.append(' '.join((str(arg) for arg in args)) + end)
+def print(*values: object, sep: Text = ' ', end: Text = '\n') -> None:
+    output.append(sep.join((str(value) for value in values)) + end)
 
 
 def helper(
@@ -34,21 +33,29 @@ def helper(
     return idx, value, output_str
 
 
+def print_without_line_feed(*values: object) -> None:
+    builtins.print(*values, end='')
+
+
 def parfor(
-    f: Callable[..., T],
-    args_list: Sequence[Tuple[Any]],
-    *,
-    print: Callable[[str, DefaultNamedArg(str, 'end')], None] = builtins.print  # noqa: F821
-) -> List[Union[T, Captured_Exception[T]]]:
+        f: Callable[..., T],
+        args_list: Sequence[Tuple[Any]],
+        *,
+        stdout: Optional[Callable[[Text], None]] = None,
+        stderr: Optional[Callable[[Text], None]] = None) -> List[Union[T, Captured_Exception[T]]]:
+    if stdout is None:
+        stdout = print_without_line_feed
+    if stderr is None:
+        stderr = stdout
     if pool is None:
         launch_parpool()
         assert pool is not None
     helper_args_list = ((idx, f, args) for idx, args in enumerate(args_list))
     result_dict: Dict[int, Union[T, Captured_Exception[T]]] = {}
     for idx, result, output in pool.imap_unordered(helper, helper_args_list):
-        print(output, end='')
+        stdout(output)
         if isinstance(result, Captured_Exception):
-            print('[{}]: {}'.format(idx, result))
+            stderr('[{}]: {}'.format(idx, result))
         result_dict[idx] = result
     result_list = [result_dict[idx] for idx in range(len(args_list))]
     return result_list
