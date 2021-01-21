42443de909ca355ddf366ddccabe120e7f6e41c3
Andy <3772259@gmail.com>
2021-01-21 17:31:31 +0800

    DepType impl

--- overloads/dyn_typing.py
+++ overloads/dyn_typing.py
@@ -2,14 +2,18 @@
 from __future__ import annotations
 
 import abc as _abc
+from typing import Any as _Any
 from typing import Callable as _Callable
 from typing import Dict as _Dict
 from typing import Optional as _Optional
-from typing import Union
+from typing import Tuple as _Tuple
+from typing import Type as _Type
 from typing import Union as _Union
 
+import numpy as _numpy
 
-def _get_value(value: _Union[int, float, DepSize]) -> Union[int, float]:
+
+def _get_value(value: _Union[int, float, DepSize]) -> _Union[int, float]:
     if isinstance(value, (int, float)):
         return value
     else:
@@ -45,97 +49,97 @@ class _DepSize(metaclass=_abc.ABCMeta):
         pass
 
     @_abc.abstractmethod
-    def get_value(self) -> Union[int, float]:
+    def get_value(self) -> _Union[int, float]:
         pass
 
 
 class DepSize(_DepSize, metaclass=_abc.ABCMeta):  # 算术运算支持
     def __neg__(self) -> SizeExpr:
-        def neg() -> Union[int, float]:
+        def neg() -> _Union[int, float]:
             return -self.get_value()
 
         return SizeExpr(_make_using(self, None), neg)
 
     def __add__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def add() -> Union[int, float]:
+        def add() -> _Union[int, float]:
             return self.get_value() + _get_value(value)
 
         return SizeExpr(_make_using(self, value), add)
 
     def __sub__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def sub() -> Union[int, float]:
+        def sub() -> _Union[int, float]:
             return self.get_value() - _get_value(value)
 
         return SizeExpr(_make_using(self, value), sub)
 
     def __mul__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def mul() -> Union[int, float]:
+        def mul() -> _Union[int, float]:
             return self.get_value() * _get_value(value)
 
         return SizeExpr(_make_using(self, value), mul)
 
     def __floordiv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def floordiv() -> Union[int, float]:
+        def floordiv() -> _Union[int, float]:
             return self.get_value() // _get_value(value)
 
         return SizeExpr(_make_using(self, value), floordiv)
 
     def __truediv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def truediv() -> Union[int, float]:
+        def truediv() -> _Union[int, float]:
             return self.get_value() / _get_value(value)
 
         return SizeExpr(_make_using(self, value), truediv)
 
     def __mod__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def mod() -> Union[int, float]:
+        def mod() -> _Union[int, float]:
             return self.get_value() % _get_value(value)
 
         return SizeExpr(_make_using(self, value), mod)
 
     def __pow__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def pow() -> Union[int, float]:
+        def pow() -> _Union[int, float]:
             return self.get_value()**_get_value(value)
 
         return SizeExpr(_make_using(self, value), pow)
 
     def __radd__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def radd() -> Union[int, float]:
+        def radd() -> _Union[int, float]:
             return _get_value(value) + self.get_value()
 
         return SizeExpr(_make_using(self, value), radd)
 
     def __rsub__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def rsub() -> Union[int, float]:
+        def rsub() -> _Union[int, float]:
             return _get_value(value) - self.get_value()
 
         return SizeExpr(_make_using(self, value), rsub)
 
     def __rmul__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def rmul() -> Union[int, float]:
+        def rmul() -> _Union[int, float]:
             return _get_value(value) * self.get_value()
 
         return SizeExpr(_make_using(self, value), rmul)
 
     def __rfloordiv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def rfloordiv() -> Union[int, float]:
+        def rfloordiv() -> _Union[int, float]:
             return _get_value(value) // self.get_value()
 
         return SizeExpr(_make_using(self, value), rfloordiv)
 
     def __rtruediv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def rtruediv() -> Union[int, float]:
+        def rtruediv() -> _Union[int, float]:
             return _get_value(value) / self.get_value()
 
         return SizeExpr(_make_using(self, value), rtruediv)
 
     def __rmod__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def rmod() -> Union[int, float]:
+        def rmod() -> _Union[int, float]:
             return _get_value(value) % self.get_value()
 
         return SizeExpr(_make_using(self, value), rmod)
 
     def __rpow__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
-        def rpow() -> Union[int, float]:
+        def rpow() -> _Union[int, float]:
             return _get_value(value)**self.get_value()
 
         return SizeExpr(_make_using(self, value), rpow)
@@ -143,9 +147,12 @@ class DepSize(_DepSize, metaclass=_abc.ABCMeta):  # 算术运算支持
 
 class SizeExpr(DepSize):
     using: _Dict[int, SizeVar]
-    expr: _Optional[_Callable[[], Union[int, float]]]
+    expr: _Optional[_Callable[[], _Union[int, float]]]
 
-    def __init__(self, using: _Dict[int, SizeVar], expr: _Callable[[], Union[int, float]]) -> None:
+    def __init__(
+            self,
+            using: _Dict[int, SizeVar],  # force line wrap
+            expr: _Callable[[], _Union[int, float]]) -> None:
         self.using = using
         self.expr = expr
 
@@ -157,7 +164,7 @@ class SizeExpr(DepSize):
         for s in self.using.values():
             s.check_using()
 
-    def get_value(self) -> Union[int, float]:
+    def get_value(self) -> _Union[int, float]:
         assert self.expr is not None
         return self.expr()
 
@@ -171,7 +178,7 @@ class SizeVar(DepSize):
         return self.value == value
 
     def check_using(self) -> None:
-        assert self.value is None
+        assert self.value is not None
 
     def get_value(self) -> int:
         assert self.value is not None
@@ -194,6 +201,176 @@ class SizeConst(DepSize):
         return self.value
 
 
+def _get_using(value: DepSize) -> _Dict[int, SizeVar]:
+    using: _Dict[int, SizeVar]
+    if isinstance(value, SizeVar):
+        using = {id(value): value}
+    elif isinstance(value, SizeExpr):
+        using = value.using
+    else:
+        using = {}
+    return using
+
+
+class DepType(metaclass=_abc.ABCMeta):
+    using: _Dict[int, SizeVar]
+
+    @_abc.abstractmethod
+    def isinstance(self, value: _Any) -> bool:
+        pass
+
+
+class NDArray(DepType):
+    dtype: _Type[_Any]
+    shape: _Tuple[DepSize, ...]
+    isfortran: bool
+
+    def __init__(self,
+                 dtype: _Type[_Any],
+                 shape: _Tuple[DepSize, ...],
+                 isfortran: bool = False) -> None:
+        self.dtype = dtype
+        self.shape = shape
+        self.isfortran = isfortran
+        using: _Dict[int, SizeVar] = {}
+        for s in shape:
+            using.update(_get_using(s))
+        self.using = using
+
+    def isinstance(self, value: _Any) -> bool:
+        if not isinstance(value, _numpy.ndarray):
+            return False
+        if _numpy.isfortran(value) != self.isfortran:  # type: ignore
+            return False
+        if value.dtype.type != self.dtype:
+            return False
+        if len(value.shape) != len(self.shape):
+            return False
+        for a, b in zip(value.shape, self.shape):
+            if isinstance(b, SizeExpr):
+                b.check_using()
+            if a != b:
+                return False
+        return True
+
+
+class Union(DepType):
+    dtype: _Tuple[DepType, ...]
+
+    def __init__(self, *dtype: DepType) -> None:
+        self.dtype = dtype
+        using: _Dict[int, SizeVar] = {}
+        for t in dtype:
+            using.update(t.using)
+        self.using = using
+
+    def isinstance(self, value: _Any) -> bool:
+        for t in self.dtype:
+            if t.isinstance(value):
+                return True
+        return False
+
+
+class List(DepType):
+    dtype: DepType
+    len: DepSize
+
+    def __init__(self, dtype: DepType, len: DepSize) -> None:
+        self.dtype = dtype
+        self.len = len
+        using: _Dict[int, SizeVar] = {}
+        using.update(dtype.using)
+        using.update(_get_using(len))
+        self.using = using
+
+    def isinstance(self, value: _Any) -> bool:
+        if not isinstance(value, list):
+            return False
+        if isinstance(self.len, SizeExpr):
+            self.len.check_using()
+        if len(value) != self.len:
+            return False
+        for v in value:
+            if not self.dtype.isinstance(v):
+                return False
+        return True
+
+
+class Tuple(DepType):
+    dtype: _Tuple[DepType, ...]
+
+    def __init__(self, dtype: _Tuple[DepType, ...], len: DepSize) -> None:
+        self.dtype = dtype
+        using: _Dict[int, SizeVar] = {}
+        for t in dtype:
+            using.update(t.using)
+        self.using = using
+
+    def isinstance(self, value: _Any) -> bool:
+        if not isinstance(value, tuple):
+            return False
+        if len(value) != len(self.dtype):
+            return False
+        for v, t in zip(value, self.dtype):
+            if not t.isinstance(v):
+                return False
+        return True
+
+
+class Dict(DepType):
+    dtype: _Dict[_Any, DepType]
+
+    def __init__(self, dtype: _Dict[_Any, DepType]) -> None:
+        self.dtype = dtype
+        using: _Dict[int, SizeVar] = {}
+        for t in dtype.values():
+            using.update(t.using)
+        self.using = using
+
+    def isinstance(self, value: _Any) -> bool:
+        if not isinstance(value, dict):
+            return False
+        if value.keys() != self.dtype.keys():
+            return False
+        for k in self.dtype.keys():
+            if not self.dtype[k].isinstance(value[k]):
+                return False
+        return True
+
+
+class Class(DepType):
+    dtype: _Type[_Any]
+
+    def __init__(self, dtype: _Type[_Any]) -> None:
+        self.dtype = dtype
+        self.using = {}
+
+    def isinstance(self, value: _Any) -> bool:
+        return isinstance(value, self.dtype)
+
+
+class Int(DepType):
+    dtype: _Type[_Any]
+
+    def __init__(self) -> None:
+        self.dtype = int
+        self.using = {}
+
+    def isinstance(self, value: _Any) -> bool:
+        return isinstance(value, self.dtype)
+
+
+class Float(DepType):
+    dtype: _Type[_Any]
+
+    def __init__(self) -> None:
+        self.dtype = float
+        self.using = {}
+
+    def isinstance(self, value: _Any) -> bool:
+        return isinstance(value, self.dtype)
+
+
 if __name__ == '__main__':
     A = SizeVar()
     assert 5 == A
@@ -206,3 +383,15 @@ if __name__ == '__main__':
     print((2**A).get_value())
     assert ee is not None
     assert 1 == 1.0
+    M = SizeVar()
+    N = SizeVar()
+    K = SizeVar()
+    B, typeB = _numpy.zeros((10, 5)), NDArray(_numpy.float64, (M, K))
+    C, typeC = _numpy.zeros((5, 7)), NDArray(_numpy.float64, (K, N))
+    CC = _numpy.zeros((6, 7))
+    assert typeB.isinstance(B)
+    assert typeC.isinstance(C)
+    assert not typeC.isinstance(CC)
+    print(K.value)
+    assert NDArray(_numpy.float64, (K + 1, N)).isinstance(CC)
+    print('using' in typeB.__dict__, 'dtype' in typeB.__dict__)
