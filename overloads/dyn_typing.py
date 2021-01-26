59e6ebb8ab0271b2802c646a0ed49f5281b1bbb2
Andy <3772259@gmail.com>
2021-01-26 22:22:51 +0800

    refactor dyn._get_value

--- overloads/dyn_typing.py
+++ overloads/dyn_typing.py
@@ -18,8 +18,15 @@ import numpy as _numpy
 def _get_value(value: _Union[int, float, DepSize]) -> _Union[int, float]:
     if isinstance(value, (int, float)):
         return value
+    elif isinstance(value, SizeConst):
+        return value.value
+    elif isinstance(value, SizeVar):
+        assert value.value is not None
+        return value.value
     else:
-        return value.get_value()
+        assert isinstance(value, SizeExpr)
+        assert value.expr is not None
+        return value.expr()
 
 
 def _make_using(
@@ -46,99 +53,95 @@ class _DepSize(metaclass=_abc.ABCMeta):
     def _eq_(self, value: int) -> bool:
         pass  # pragma: no cover
 
-    @_abc.abstractmethod
-    def get_value(self) -> _Union[int, float]:
-        pass  # pragma: no cover
-
 
 class DepSize(_DepSize, metaclass=_abc.ABCMeta):  # 算术运算支持
     def __neg__(self) -> SizeExpr:
         def neg() -> _Union[int, float]:
-            return -self.get_value()
+            return -_get_value(self)
 
         return SizeExpr(_make_using(self, None), neg)
 
     def __add__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def add() -> _Union[int, float]:
-            return self.get_value() + _get_value(value)
+            return _get_value(self) + _get_value(value)
 
         return SizeExpr(_make_using(self, value), add)
 
     def __sub__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def sub() -> _Union[int, float]:
-            return self.get_value() - _get_value(value)
+            return _get_value(self) - _get_value(value)
 
         return SizeExpr(_make_using(self, value), sub)
 
     def __mul__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def mul() -> _Union[int, float]:
-            return self.get_value() * _get_value(value)
+            return _get_value(self) * _get_value(value)
 
         return SizeExpr(_make_using(self, value), mul)
 
     def __floordiv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def floordiv() -> _Union[int, float]:
-            return self.get_value() // _get_value(value)
+            return _get_value(self) // _get_value(value)
 
         return SizeExpr(_make_using(self, value), floordiv)
 
     def __truediv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def truediv() -> _Union[int, float]:
-            return self.get_value() / _get_value(value)
+            return _get_value(self) / _get_value(value)
 
         return SizeExpr(_make_using(self, value), truediv)
 
     def __mod__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def mod() -> _Union[int, float]:
-            return self.get_value() % _get_value(value)
+            return _get_value(self) % _get_value(value)
 
         return SizeExpr(_make_using(self, value), mod)
 
     def __pow__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def pow() -> _Union[int, float]:
-            return self.get_value()**_get_value(value)
+            return _get_value(self)**_get_value(value)
 
         return SizeExpr(_make_using(self, value), pow)
 
     def __radd__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def radd() -> _Union[int, float]:
-            return _get_value(value) + self.get_value()
+            return _get_value(value) + _get_value(self)
 
         return SizeExpr(_make_using(self, value), radd)
 
     def __rsub__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def rsub() -> _Union[int, float]:
-            return _get_value(value) - self.get_value()
+            return _get_value(value) - _get_value(self)
 
         return SizeExpr(_make_using(self, value), rsub)
 
     def __rmul__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def rmul() -> _Union[int, float]:
-            return _get_value(value) * self.get_value()
+            return _get_value(value) * _get_value(self)
 
         return SizeExpr(_make_using(self, value), rmul)
 
     def __rfloordiv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def rfloordiv() -> _Union[int, float]:
-            return _get_value(value) // self.get_value()
+            return _get_value(value) // _get_value(self)
 
         return SizeExpr(_make_using(self, value), rfloordiv)
 
     def __rtruediv__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def rtruediv() -> _Union[int, float]:
-            return _get_value(value) / self.get_value()
+            return _get_value(value) / _get_value(self)
 
         return SizeExpr(_make_using(self, value), rtruediv)
 
     def __rmod__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def rmod() -> _Union[int, float]:
-            return _get_value(value) % self.get_value()
+            return _get_value(value) % _get_value(self)
 
         return SizeExpr(_make_using(self, value), rmod)
 
     def __rpow__(self, value: _Union[int, float, DepSize]) -> SizeExpr:
         def rpow() -> _Union[int, float]:
-            return _get_value(value)**self.get_value()
+            return _get_value(value)**_get_value(self)
 
         return SizeExpr(_make_using(self, value), rpow)
 
@@ -162,10 +165,6 @@ class SizeExpr(DepSize):
         for s in self.using.values():
             s.check_using()
 
-    def get_value(self) -> _Union[int, float]:
-        assert self.expr is not None
-        return self.expr()
-
 
 class SizeVar(DepSize):
     value: _Optional[int] = None
@@ -178,10 +177,6 @@ class SizeVar(DepSize):
     def check_using(self) -> None:
         assert self.value is not None
 
-    def get_value(self) -> int:
-        assert self.value is not None
-        return self.value
-
 
 class SizeConst(DepSize):
     value: int
@@ -191,9 +186,6 @@ class SizeConst(DepSize):
 
     def _eq_(self, value: int) -> bool:
         return self.value == value
-
-    def get_value(self) -> int:
-        return self.value
 
 
 def _get_using(value: DepSize) -> _Dict[int, SizeVar]:
