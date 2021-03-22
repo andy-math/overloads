4818e028489d1a2e5af4dd04c6940d846bc524cf
Andy <72276976+Andy-math@users.noreply.github.com>
2021-03-22 22:21:04 +0800

    reformat

--- overloads/tuplize.py
+++ overloads/tuplize.py
@@ -4,19 +4,21 @@ from typing import Callable as _Callable
 from typing import Tuple as _Tuple
 from typing import TypeVar as _TypeVar
 
-_T1 = _TypeVar('_T1')
-_T2 = _TypeVar('_T2')
-_T3 = _TypeVar('_T3')
-_T4 = _TypeVar('_T4')
-_T5 = _TypeVar('_T5')
-_T6 = _TypeVar('_T6')
-_T7 = _TypeVar('_T7')
-_T8 = _TypeVar('_T8')
-_T9 = _TypeVar('_T9')
-_return_t = _TypeVar('_return_t')
+_T1 = _TypeVar("_T1")
+_T2 = _TypeVar("_T2")
+_T3 = _TypeVar("_T3")
+_T4 = _TypeVar("_T4")
+_T5 = _TypeVar("_T5")
+_T6 = _TypeVar("_T6")
+_T7 = _TypeVar("_T7")
+_T8 = _TypeVar("_T8")
+_T9 = _TypeVar("_T9")
+_return_t = _TypeVar("_return_t")
 
 
-def _tuplize_unsafe(f: _Callable[..., _return_t]) -> _Callable[[_Tuple[_Any, ...]], _return_t]:
+def _tuplize_unsafe(
+    f: _Callable[..., _return_t]
+) -> _Callable[[_Tuple[_Any, ...]], _return_t]:
     @_wraps(f)
     def wrapper(args: _Tuple[_Any, ...]) -> _return_t:
         return f(*args)
@@ -32,12 +34,15 @@ def tuplize_1(f: _Callable[[_T1], _return_t]) -> _Callable[[_Tuple[_T1]], _retur
     return _tuplize_unsafe(f)
 
 
-def tuplize_2(f: _Callable[[_T1, _T2], _return_t]) -> _Callable[[_Tuple[_T1, _T2]], _return_t]:
+def tuplize_2(
+    f: _Callable[[_T1, _T2], _return_t]
+) -> _Callable[[_Tuple[_T1, _T2]], _return_t]:
     return _tuplize_unsafe(f)
 
 
 def tuplize_3(
-        f: _Callable[[_T1, _T2, _T3], _return_t]) -> _Callable[[_Tuple[_T1, _T2, _T3]], _return_t]:
+    f: _Callable[[_T1, _T2, _T3], _return_t]
+) -> _Callable[[_Tuple[_T1, _T2, _T3]], _return_t]:
     return _tuplize_unsafe(f)
 
 
